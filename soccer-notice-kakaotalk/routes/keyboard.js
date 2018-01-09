27146a8a954d572c625a49038de40d6603ce7fa0
leesanghoon <dl57934@naver.com>
2018-01-09 20:37:11 +0900

    keyboard.js추가

--- soccer-notice-kakaotalk/routes/keyboard.js
+++ soccer-notice-kakaotalk/routes/keyboard.js
@@ -2,5 +2,11 @@ var express = require('express');
 var router = express.Router();
 var keyboard = {"type" : "buttons",
     "buttons" : ["선택 1", "선택 2", "선택 3"]};
-router.send(keyboard);
+var app = express();
+app.use(function (req,res,next) {
+   res.writeHead('200', {'Content-Type':'application/json;charset=utf8'});
+   res.write(keyboard);
+   res.end();
+});
+
 module.exports = router;
