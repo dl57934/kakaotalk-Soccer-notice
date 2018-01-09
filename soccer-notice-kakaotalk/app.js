ef811119480d3af97f7b2a987e78a6c984e504c4
root <root@ip-172-31-14-27.ap-northeast-2.compute.internal>
2018-01-09 14:27:51 +0000

    텍스트로변경

--- soccer-notice-kakaotalk/app.js
+++ soccer-notice-kakaotalk/app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', keyboard);
+app.use('/keyboard', keyboard);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- soccer-notice-kakaotalk/routes/keyboard.js
+++ soccer-notice-kakaotalk/routes/keyboard.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var keyboard = {"type" : "buttons",
-    "buttons" : ["선택 1", "선택 2", "선택 3"]};
+var keyboard = {"type" : "text"};
 var app = express();
-router.get('/keyboard',function (req,res,next){
+router.get('/',function (req,res,next){
   res.set('200',{'Content-Type':'application/json;charset=utf8'});
   res.send(keyboard);
 });
