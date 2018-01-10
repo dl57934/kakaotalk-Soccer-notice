c4062b45fadc181a9919a8fcb3b4f3bbbeefd53b
leesanghoon <dl57934@naver.com>
2018-01-10 20:08:07 +0900

    cotent 가져오기

--- soccer-notice-kakaotalk/routes/message.js
+++ soccer-notice-kakaotalk/routes/message.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var page = require('webpage').create();
-var title;
+var phantom = require('phantom');
+var _ph, _page, _outObj;
 router.post('/',function (req,res,next) {
     const _obj ={
         "user_key":req.body.user_key,
@@ -10,14 +10,24 @@ router.post('/',function (req,res,next) {
     };
     var bye_message = {"message":{"text":"hihi"}};
     if(req.body.content == "안녕") {
-        page.open('http://sports.news.naver.com/wfootball/index.nhn',function (status) {
-           title = page.evaluate(function () {
-              return document.title;
-           });
-        });
-        res.set('200', {'Content-Type': 'application/json;charset=utf8'});
-        res.send(title);
-        phantom.exit();
+        phantom.create().then(function (ph) {
+            _ph = ph;
+            return _ph.createPage();
+        }).then(function (page) {
+            _page = page;
+            return _page.open('http://sports.news.naver.com/wfootball/index.nhn');
+        }).then(function (status) {
+            console.log(status);
+            return _page.property('content');
+        }).then(function (content) {
+            console.log(content);
+            res.set('200', {'Content-Type': 'application/json;charset=utf8'});
+            res.send(content);
+            _page.close();
+            _ph.exit();
+        }).catch(function (e) {
+            console.log(e);
+        })
     }
 });
 
