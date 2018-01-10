87e04686b69bf93b599e7670c6586e689c6e9a0c
leesanghoon <dl57934@naver.com>
2018-01-10 19:35:08 +0900

    title 가져오기

--- soccer-notice-kakaotalk/routes/message.js
+++ soccer-notice-kakaotalk/routes/message.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var page = require('webpage').create();
+var title;
 router.post('/',function (req,res,next) {
     const _obj ={
         "user_key":req.body.user_key,
@@ -9,8 +10,13 @@ router.post('/',function (req,res,next) {
     };
     var bye_message = {"message":{"text":"hihi"}};
     if(req.body.content == "안녕") {
+        page.open('http://sports.news.naver.com/wfootball/index.nhn',function (status) {
+           title = page.evaluate(function () {
+              return document.title;
+           });
+        });
         res.set('200', {'Content-Type': 'application/json;charset=utf8'});
-        res.send(bye_message);
+        res.send(title);
     }
 });
 
