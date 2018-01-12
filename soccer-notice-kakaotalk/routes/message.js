5d25946f792d541a02fea730fa4418cbf69bda00
leesanghoon <dl57934@naver.com>
2018-01-12 14:44:27 +0900

    블로그용 2

--- soccer-notice-kakaotalk/routes/message.js
+++ soccer-notice-kakaotalk/routes/message.js
@@ -3,35 +3,87 @@ var router = express.Router();
 var phantom = require('phantom');
 var _ph, _page, _outObj;
 router.get('/',function (req,res,next) {
-
-
+    if(req.body.content == "E 순위"||req.body.content == "E순위" || req.body.content == "L 순위"||req.body.content == "L순위" ||req.body.content == "B 순위"||req.body.content == "B순위" ) {
         phantom.create().then(function (ph) {
             _ph = ph;
             return _ph.createPage();
         }).then(function (page) {
             var selectPage;
-            /*if(req.body.content == "E 순위"||req.body.content == "E순위")
+            if(req.body.content == "E 순위"||req.body.content == "E순위")
                 selectPage = 'http://sports.news.naver.com/wfootball/record/index.nhn';
             else if( req.body.content == "L 순위"||req.body.content == "L순위")
                 selectPage = 'http://sports.news.naver.com/wfootball/record/index.nhn?category=primera&tab=team';
             else if(req.body.content == "B 순위"||req.body.content == "B순위")
-                selectPage = 'http://sports.news.naver.com/wfootball/record/index.nhn?category=bundesliga&tab=team';*/
+                selectPage = 'http://sports.news.naver.com/wfootball/record/index.nhn?category=bundesliga&tab=team';
             _page = page;
-            return _page.open('http://sports.news.naver.com/wfootball/record/index.nhn?category=bundesliga&tab=team');
+            return _page.open(selectPage);
         }).then(function (status) {
             console.log(status);
-	    return document.textContent;
-        }).then(function (content) {   
-	    content = content.replace(/^\s+|\s+$/gm,'');
-	    var bye_message = {"message":{"text":content.toString()}};
+            return  _page.evaluate(function(){
+                var tdLength = document.getElementsByTagName('td');
+                var returnText="";
+                var ranking =1;
+                for(var i = 0; i < tdLength.length;i++){
+                    if(i % 10 == 1){
+                        returnText +=ranking+'위' + tdLength[i].textContent+'\n';
+                        ranking++;
+                    }
+                }
+                return returnText;
+            })
+        }).then(function (content) {
+            content = content.replace(/^\s+|\s+$/gm,'');
+            var bye_message = {"message":{"text":content.toString()}};
             console.log(content);
             _page.close();
             _ph.exit();
+            res.set('200', {'Content-Type': 'application/json;charset=utf8'});
+            res.send(bye_message);
+
         }).catch(function (e) {
             console.log(e);
         })
+    }
 
-
+    else if(req.body.content == "E 일정"||req.body.content == "E일정" || req.body.content == "L 일정"||req.body.content == "L일정" ||req.body.content == "B 일정"||req.body.content == "B일정"){
+        phantom.create().then(function (ph) {
+            _ph = ph;
+            return _ph.createPage();
+        }).then(function (page) {
+            var selectPage;
+            if(req.body.content == "E 일정"||req.body.content == "E일정")
+                selectPage = 'http://sports.news.naver.com/wfootball/schedule/index.nhn';
+            else if( req.body.content == "L 일정"||req.body.content == "L일정")
+                selectPage = 'http://sports.news.naver.com/wfootball/schedule/index.nhn?category=primera';
+            else if(req.body.content == "B 일정"||req.body.content == "B일정")
+                selectPage = 'http://sports.news.naver.com/wfootball/schedule/index.nhn?category=bundesliga';
+            _page = page;
+            return _page.open(selectPage);
+        }).then(function (status) {
+            return _page.evaluate(function () {
+                var tdLength = document.getElementsByTagName('tbody')[0].textContent;
+                var returnText="";
+                /*for(var i = 0; i <tdLength.length; i++){
+                    if(i%4 == 0 || i%4 == 1)
+                    returnText += tdLength[i].textContent;
+                }*/
+                return tdLength;
+            })
+        }).then(function (content) {
+            content = content.replace(/^\s+|\s+$/gm,'');
+            content = content.replace(/경기기록/g,'');
+            content = content.replace(/경기영상/g,'');
+            content = content.replace(/전력비교/g,'');
+            var bye_message = {"message":{"text":content.toString()}};
+            console.log(content);
+            _page.close();
+            _ph.exit();
+            res.set('200', {'Content-Type': 'application/json;charset=utf8'});
+            res.send(bye_message);
+        })
+    }
 });
 
 module.exports = router;
+
+
